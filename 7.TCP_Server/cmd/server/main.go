f90f85f638c33854f6c754db58436f1266019ff8
ibLu247 <161561405+ibLu247@users.noreply.github.com>
2026-04-27 17:23:00 +0400

    Delete 7.TCP_Server directory

--- 7.TCP_Server/cmd/server/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "gitlab.com/mediasoft-internship/practice2024/less7/homework/internal/server"
-
-func main() {
-	server.Start()
-}
--- 7.TCP_Server/go.mod
+++
@@ -1,3 +0,0 @@
-module gitlab.com/mediasoft-internship/practice2024/less7/homework
-
-go 1.26.2
